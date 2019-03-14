14299ffe62644dea1fb1e2306fd67d743326a6dc
xiaoming <xiaoming@qq.com>
2019-03-14 23:40:08 +0800

    1

--- src/components/CmtList.jsx
+++ src/components/CmtList.jsx
@@ -13,7 +13,8 @@ export default class CmtList extends React.Component {
                 {id: 3, user: "王五", content: "哈哈，凉席"},
                 {id: 4, user: "赵六", content: "哈哈，小崽子"},
                 {id: 5, user: "田七", content: "哈哈，不会啊"},
-            ]
+            ],
+            msg: "123"
         }
     }
 
@@ -22,6 +23,37 @@ export default class CmtList extends React.Component {
         return <div>
             <h1>这是评论列表组件</h1>
             {this.state.CommentList.map(item => <CmtItem {...item} key={item.id}></CmtItem>)}
+
+            <button onClick={this.run}>按钮</button>
+
+            <button onClick={this.xg}>修改</button>
+            <p>{this.state.msg}</p>
+
+            <input type="text" value={this.state.msg} onChange={(e) => this.txtchange(e)}/>
         </div>
     }
+
+
+    /*run(){
+         console.log("我要跑")
+    }*/
+    run = () => {
+        console.log("跑起来")
+    };
+
+    xg = () => {
+        this.setState({
+            msg:"修改了"
+        },function () {
+            //修改后想立刻拿到最新的值
+        })
+    };
+
+    txtchange = (e) => {
+        const newVal = e.target.value;
+        this.setState({
+            msg:newVal
+        })
+    }
+
 }
